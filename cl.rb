7d092a44ab23c7c1d6e57503e07143e683171303
mironovd <mironovd@remilia.poncelet.ru>
2013-01-06 17:35:20 +0400

    Modularization of seeds

--- cl.rb
+++ cl.rb
@@ -2,162 +2,22 @@
 #require 'awesome_print'
 require 'set'
 
-def dc4(value)
-  if value.is_a?(Hash)
-    result = value.clone
-    value.each{|k, v| result[k] = dc4(v)}
-    result
-  elsif value.is_a?(Array)
-    result = value.clone
-    result.clear
-    value.each{|v| result << dc4(v)}
-    result
-  else
-    value
-  end
-end
-
-
-class Tableau
-    attr_accessor :deg
-    attr_accessor :tab
-
-    def initialize n
-  @deg=n
-	@tab=[]
-	(0..(n-1)).each{|i| @tab[i]=[]}
-	(0..(n-1)).each{|i| (0..(n-1)).each{|j| @tab[i][j]=0 }}
-    end
-
-    def == other
-	(self.deg==other.deg) and (self.tab==other.tab)
-    end
-
-    def + other
-	x=Tableau.new(self.deg)
-	(0..(@deg-1)).each{|i| (0..(@deg-1)).each{|j| x.tab[i][j]=self.tab[i][j]+other.tab[i][j]}}
-	return x
-    end
-
-    def - other
-	x=Tableau.new(self.deg)
-	(0..(@deg-1)).each{|i| (0..(@deg-1)).each{|j| x.tab[i][j]=self.tab[i][j]-other.tab[i][j]}}
-	return x
-    end
-
-    def -@
-	x=Tableau.new(self.deg)
-	(0..(@deg-1)).each{|i| (0..(@deg-1)).each{|j| x[i][j]=-self.tab[i][j]}}
-	return x
-    end	
-
-    def <=> other
-	return self.tab.flatten <=> other.tab.flatten
-    end
-
-    def eql? other
-	return self == other
-    end
-
-    def positive?
-	return (not (self.tab.flatten.find{|i| i<0}))
-    end
-    
-    def hash
-	return self.tab.hash
-    end
-
-end
-
-class Graph 
-    attr_accessor :edges
-    attr_accessor :vertices
-    attr_accessor :deg
-    attr_accessor :tabs
-
-    attr_accessor :hash
-
-    def initialize n
-	@edges=Hash.new
-	@vertices=Array.new
-	@tabs=Hash.new
-	(0..(n-1)).each{|i| (0..(n-i-1)).each{|j| @vertices.push([i,j])}}
-	(0..(n-2)).each{|i| (0..(n-i-2)).each{|j| @edges[[[i,j],[i+1,j]]]=1; @edges[[[i+1,j],[i,j]]]=-1;
-			@edges[[[i,j+1],[i,j]]]=1; @edges[[[i,j],[i,j+1]]]=-1;
-			@edges[[[i+1,j],[i,j+1]]]=1; @edges[[[i,j+1],[i+1,j]]]=-1;
-	}}
-	(0..(n-1)).each{|i| (0..(n-i-1)).each{|j| 
-		    x=Tableau.new(n)
-		    (0..j).each{|p| x.tab[p][p+i]=1}
-		    @tabs[[i,j]]=x
-#		    puts @tabs[[i,j]]
-	}}
-	@deg=n
-	rehash
-    end
-
-    def mutable
-	@vertices.find_all{|e| e[0]!=0 and e[0]+e[1]<@deg-1}
-    end
-
-    def In vertex
-	@edges.keys.find_all{|k| @edges[k]>0}.find_all{|k| k[1]==vertex}
-    end
-
-    def Out vertex
-	@edges.keys.find_all{|k| @edges[k]>0}.find_all{|k| k[0]==vertex}
-    end
-
-    def mutate vertex
-#	x=dc4(self)
-	z=dc4(@edges)
-
-#	ap self.inspect
-#	ap In(vertex).inspect
-#	ap Out(vertex).inspect
-
-	a=@tabs[vertex]
-	b1=In(vertex).reduce(Tableau.new(@deg)){|t, e| 
-#	    puts e.inspect, @tabs[e[0]].inspect; 
-	    t+=@tabs[e[0]]}
-	b2=Out(vertex).reduce(Tableau.new(@deg)){|t, e| t+=@tabs[e[1]]}
-	b=[b1,b2].max-a
-
-	In(vertex).each{|i| Out(vertex).each{|o|
-	    z[[i[0],o[1]]]=(z[[i[0],o[1]]] ? z[[i[0],o[1]]] : 0)+z[[i[0], vertex]] * z[[vertex,o[1]]]
-	    z[[o[1],i[0]]]=-z[[i[0],o[1]]]
-	}}
-	(In(vertex)+Out(vertex)).each{|e| p=z[e]; z[e]=-p; z[e.reverse]=p;}
-	x=Graph.new(@deg)
-	x.edges=z.reject{|k,v| v==0}
-	x.tabs=dc4(@tabs)
-	x.tabs[vertex]=b
-	x.rehash
-	return x
-    end
-
-    def rehash
-	@hash=self.mutable.inject([]){|a,v| a.push(self.tabs[v])}.sort.inject([]){|a,t| a.push(t.hash)}.hash
-    end
-
-    def == (other)
-#	return (self.deg==other.deg and self.vertices==other.vertices and self.edges==other.edges)		
-	(self.deg==other.deg) and self.hash==other.hash and self.mutable.inject([]){|a,v| a.push(self.tabs[v])}.sort==other.mutable.inject([]){|a,v| a.push(other.tabs[v])}.sort
-#	(self.deg==other.deg) and self.tabs.values.sort == other.tabs.values.sort
-    end
-
-	
-
-end
+require_relative 'seeds'
 
 Gs=Graph.new(ARGV[0].to_i)
+
+$prefix="cl."+ARGV[0]+".res"#ARGV[1]
+
+`rm -fr #{$prefix}.neggr`
+Dir.mkdir($prefix+".neggr")
 
 #puts Gs.mutable.inspect
 
 St=Array.new
-As=Array.new
-St[0]=[Gs]
-As[0]=[""]
+Sh=Hash.new
+#As=Hash.new
+St[0]=[{:g=>Gs, :path=>[]}]
+Sh[Gs.hash]=[Gs]
 
 Tabs=Set.new
 
@@ -165,30 +25,50 @@ Gs.mutable.each{|v| Tabs.add(Gs.tabs[v])}
 
 def Step(n)
     i=1
-    As[n+1]=[]
+#    As[n+1]=[]
     St[n+1]=[]
     (St[n]).each{|r|
-	r.mutable.each{|v|
+	r[:g].mutable.each{|v|
 #	    puts v.inspect
 #	    puts "================"
-	    g=r.mutate(v);
+	    g=r[:g].mutate(v);
 #	    puts g.inspect
 	    nw=true
 #	    (0..n).each{|nn| (0..St[nn].size-1).each{|u| 
 #		gg=St[nn][u]
 #		puts gg.inspect
-	    (0..n+1).each{|nn| St[nn].each{|gg| 
+#	    (0..n+1).each_with_index{|nn,u| St[nn].each{|gg| 
+#
+#		if nw and gg[:g]==g then
+#		    nw=false
+#		    As[[i,v]]={:s=>nn,:n=>u}
+#		    #[n+1].push({:g=>i, :v => v, :gt=>[nn,u]})
+#		end
+#	    }}
 
-		if nw and gg==g then
-		    nw=false
-#		    As[n+1].push({:g=>i, :v => v, :gt=>[nn,u]})
-		end
-	    }}
+	    if Sh[g.hash] then
+		Sh[g.hash].each{|gg|
+		    if nw and gg==g then
+			nw = false
+		    end
+		}
+	    end
+
 	    if nw then
-		St[n+1].push(g)
-		Tabs.add(g.tabs[v])
-		if not g.tabs[v].positive? then
-		   puts "FOUND NEGATIVE:\n"+(g.Inspect)
+		gx={:g=>g,:path=>r[:path]+[v]}
+		St[n+1].push(gx)
+		if Sh[g.hash] then
+		    Sh[g.hash].push(g)
+		else
+		    Sh[g.hash]=[g]
+		end
+		
+		if Tabs.add?(gx[:g].tabs[v])
+		    File.open($prefix+".tabs","wb"){|f| Tabs.to_a.sort.each{|t| f.write(t.to_str+"\n")}}
+		end
+		if not gx[:g].tabs[v].positive? then
+		   puts "FOUND NEGATIVE:\n"+(gx.inspect)
+		    File.write($prefix+".neggr/"+"graph"+gx[:g].deg.to_s+"."+gx[:g].hash.to_s+".marshal", Marshal.dump(gx))
 		    $stdout.flush 
 		end 		
 	    end	
